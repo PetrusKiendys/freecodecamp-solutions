b8cbd7f1c6fbb0bb0fa0adae4edcdfd62d4dd2db
Petrus Kiendys <petruskiendys@gmail.com>
2016-08-08 21:23:46 +0200

    Explain implementation of 'Basic Algorithm Scripting/10-truncate-a-string.js'

--- Basic Algorithm Scripting/10-truncate-a-string.js
+++ Basic Algorithm Scripting/10-truncate-a-string.js
@@ -1,14 +1,13 @@
 // URL: https://www.freecodecamp.com/challenges/truncate-a-string
 
-// TODO: comment and explain implementation
 function truncateString(str, num) {
   var res = "";
   
-  if (num > 3 && str.length > num)
+  if (num > 3 && str.length > num)        // truncate string replacing the three last characters with dots
     res = str.slice(0, num-3) + "...";
-  else if (num <= 3 && str.length > num)
+  else if (num <= 3 && str.length > num)  // truncate string appending dots to the end of truncation
     res = str.slice(0, num) + "...";
-  else
+  else									  // if num >= str.length, return str untruncated
     res = str;
   
   return res;
